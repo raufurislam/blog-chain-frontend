d5575d1de3257c0be672ccecdbde25a0201e39c4
raufurislam <raufurislam@gmail.com>
2025-09-26 15:43:40 +0600

    feat(auth): implement user registration with server action

---
+++ src/actions/auth.ts
@@ -0,0 +1,17 @@
+"use server";
+import { FieldValues } from "react-hook-form";
+
+export async function register(data: FieldValues) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res?.ok) {
+    console.error("User Registration failed", await res.text());
+  }
+
+  return await res.json();
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/providers/AuthProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster richColors position="top-center" />
+
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
